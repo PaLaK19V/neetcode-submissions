401377fc64128a8e0ec084d89ccb44237a7807c0
PaLaK19V <palakv1905@gmail.com>
2026-05-25 11:46:47 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> map;
+        for(string str: strs){
+            string sorted=str;
+            sort(sorted.begin(),sorted.end());
+            map[sorted].push_back(str);
+        }
+        vector<vector<string>> result;
+        for(auto& pair:map){
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
