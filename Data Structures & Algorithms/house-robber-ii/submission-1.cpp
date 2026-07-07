fba630493823f0a41754ef957a0f15bf0e0ee382
PaLaK19V <palakv1905@gmail.com>
2026-07-07 17:46:17 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> memo;
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        memo.assign(n,-1);
+        int case1=dfs(nums,0,n-2);
+
+        memo.assign(n,-1);
+        int case2=dfs(nums,1,n-1);
+        return max(case1,case2);
+    }
+    int dfs(vector<int>&nums,int i,int end){
+        if(i>end) return 0;
+        if(memo[i]!=-1) return memo[i];
+        int rob=nums[i]+dfs(nums,i+2,end);
+        int skip=dfs(nums,i+1,end);
+        return memo[i]=max(rob,skip);
+    }
+};
